eaf0a8f63099275a5592bf7ea0bc92a1f54ca804
Faiz Khan <writetofaiz7@gmail.com>
2025-09-27 15:30:58 -0400

    Pixel Art

--- player.gd
+++ player.gd
@@ -2,13 +2,22 @@ extends CharacterBody2D
 
 
 const SPEED = 550.0
-const JUMP_VELOCITY = -900.0
-
+const JUMP_VELOCITY = -750.0
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
+	if (velocity.x > 1 || velocity.x < -1) && is_on_floor():
+		sprite_2d.animation = "running"
+	elif not is_on_floor():
+		sprite_2d.animation = "jumping"
+	else:
+		sprite_2d.animation = "default"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -23,3 +32,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
+	
+	
